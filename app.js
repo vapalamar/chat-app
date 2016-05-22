5a190cfe581388f450e28eecdcb8e7482aedcd7e
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-05-22 15:05:18 +0300

    Added usernames

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ var io = require ('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+io.on('connection', function(socket) {
+    console.log('User joined the chat!');
+    
+    socket.on('chat message', function(data) {
+        io.emit('chat message', {
+            msg: data,
+            nickName: socket.nickName
+        });
+    });
+    
+    socket.on('disconnect', function () {
+        console.log('User left the chat!')
+    })
+    
+    socket.on('new user', function (nickName) {
+       socket.nickName = nickName;
+       io.emit('new user', nickName); 
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -3,16 +3,34 @@
     var inputArea = document.getElementById('input-area');
     var messagesView = document.getElementById('messages');
     var messageInput = document.getElementById('message-input');
+    var loginForm = document.getElementById('login-form');
+    var loginInput = document.getElementById('login-input');
+    
+    var socketEmit = function (formEvent, eventName, input) {
+        formEvent.preventDefault();
+        socket.emit(eventName, input.value);
+        input.value = '';
+        
+        return false;
+    }
     
     inputArea.addEventListener('submit', function (e) {
-        e.preventDefault();
-        socket.emit('chat message', messageInput.value);
-        messageInput.value = '';
-        return false; 
+        socketEmit(e, 'chat message', messageInput)
     });
+    
+    loginForm.addEventListener('submit', function (e) {
+        socketEmit(e, 'new user', loginInput)
+    });
+    
     socket.addEventListener('chat message', function (data) {
         var newMsg = document.createElement('li');
-        newMsg.innerHTML = data;
+        var nickName = document.createElement('span');
+        var msg = document.createElement('p');
+        
+        nickName.innerHTML = data.nickName;
+        msg.innerHTML = data.msg;
+        newMsg.appendChild(nickName);
+        newMsg.appendChild(msg);
         messagesView.appendChild(newMsg);
     });
 })();
